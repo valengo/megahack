c8e6298cbafc6a98198a27b084544d9af8d927f2
Andressa Valengo <valengoandressa@gmail.com>
2020-05-01 13:42:58 -0300

    added inital storyboards and created mvc groups

---
+++ megahack-iOS/megahack/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  megahack
+//
+//  Created by Andressa Valengo on 01/05/20.
+//  Copyright © 2020 Andressa Valengo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
