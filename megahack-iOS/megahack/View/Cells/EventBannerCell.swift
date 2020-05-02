88c2732492892ae84b7227fcf9d6e1afc4982778
Andressa Valengo <valengoandressa@gmail.com>
2020-05-02 03:55:56 -0300

    added tab bar

---
+++ megahack-iOS/megahack/View/Cells/EventBannerCell.swift
@@ -0,0 +1,20 @@
+//
+//  EventBannerCell.swift
+//  megahack
+//
+//  Created by Andressa Valengo on 02/05/20.
+//  Copyright © 2020 Andressa Valengo. All rights reserved.
+//
+
+import UIKit
+
+class EventBannerCell: UICollectionViewCell {
+
+    @IBOutlet weak var eventName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
