f4cfdee7d8cfd95695d0cf5334588087e67485a4
Andressa Valengo <valengoandressa@gmail.com>
2020-05-02 12:26:33 -0300

    added xibs for home

---
+++ megahack-iOS/megahack/View/Cells/EventDataCell.swift
@@ -0,0 +1,18 @@
+//
+//  EventDataCell.swift
+//  megahack
+//
+//  Created by Andressa Valengo on 02/05/20.
+//  Copyright © 2020 Andressa Valengo. All rights reserved.
+//
+
+import UIKit
+
+class EventDataCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
