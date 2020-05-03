7fb57a011fa57c41a6f511a88cded41fd869353e
Andressa Valengo <valengoandressa@gmail.com>
2020-05-03 16:55:00 -0300

    suggestions

---
+++ megahack-iOS/megahack/Controllers/SuggestionsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SuggestionsViewController.swift
+//  megahack
+//
+//  Created by Andressa Valengo on 03/05/20.
+//  Copyright © 2020 Andressa Valengo. All rights reserved.
+//
+
+import UIKit
+
+class SuggestionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
