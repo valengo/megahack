be4b1a868b560d338e86e860143bc40a2709ba5e
Andressa Valengo <valengoandressa@gmail.com>
2020-05-03 13:49:58 -0300

    added profile cell

---
+++ megahack-iOS/megahack/View/Cells/ProfileCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileCell.swift
+//  megahack
+//
+//  Created by Andressa Valengo on 03/05/20.
+//  Copyright © 2020 Andressa Valengo. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
