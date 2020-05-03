adfe9975816afcb655f8c6ac214747a6e0e17bc8
Andressa Valengo <valengoandressa@gmail.com>
2020-05-03 17:55:31 -0300

    notifications

---
+++ megahack-iOS/megahack/View/Utils/NotificationManager.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationManager.swift
+//  megahack
+//
+//  Created by Andressa Valengo on 03/05/20.
+//  Copyright © 2020 Andressa Valengo. All rights reserved.
+//
+
+import UIKit
+
+class NotificationManager {
+    
+}
+
+
